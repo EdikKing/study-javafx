3944f77b65f238b96369fa34035e4cf61ba0eb8f
Edik <wfj@edik.cn>
2020-07-30 23:43:11 +0800

    添加启动类

---
+++ src/main/java/cn/edik/AppStart.java
@@ -0,0 +1,17 @@
+package cn.edik;
+
+public class AppStart {
+
+    public static void main(String[] args) {
+//      Study1_Stage.main(args);
+//      Study2_Stage.main(args);
+//      Study3_Platform.main(args);
+//      Study3_Platform2.main(args);
+//      Study4_Screen.main(args);
+//      Study5_Scene.main(args);
+//      Study6_Group.main(args);
+      Study7_Button.main(args);
+//      Study8_HotKey.main(args);
+    }
+
+}
