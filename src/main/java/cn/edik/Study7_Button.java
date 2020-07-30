2fc0b476d18e13498e4225933765f454842576ea
Edik <wfj@edik.cn>
2020-07-30 11:50:05 +0800

    Study7_Button

--- src/main/java/cn/edik/Study7_Button.java
+++ src/main/java/cn/edik/Study7_Button.java
@@ -9,6 +9,8 @@ import javafx.scene.Cursor;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Paint;
@@ -40,10 +42,8 @@ public class Study7_Button extends Application {
         button.setCursor(Cursor.HAND);
 
         button.setBorder(new Border(new BorderStroke(Paint.valueOf("#8E44AD"),
-                BorderStrokeStyle.DOTTED,new CornerRadii(10),new BorderWidths(5)
-                )));
-
-
+                BorderStrokeStyle.DOTTED, new CornerRadii(10), new BorderWidths(5)
+        )));
 
 
         Button button2 = new Button();
@@ -56,9 +56,9 @@ public class Study7_Button extends Application {
         // 设置样式
         button2.setStyle(
                 "-fx-background-color: #201bff;" +
-                "-fx-font-size: 40;" +
-                "-fx-background-radius: 10;"+
-                "-fx-text-fill: #ff3a50;"
+                        "-fx-font-size: 40;" +
+                        "-fx-background-radius: 10;" +
+                        "-fx-text-fill: #ff3a50;"
         );
 
         button2.setOnAction(new EventHandler<ActionEvent>() {
@@ -67,12 +67,12 @@ public class Study7_Button extends Application {
                 // 获取点击事件的元素
                 Button source = (Button) actionEvent.getSource();
 
-                System.out.println("点击了"+source.getText());
+                System.out.println("点击了" + source.getText());
             }
         });
 
 
-        Button button3 = new Button("按钮3");
+        Button button3 = new Button("测试鼠标双击");
         button3.setLayoutX(100);
         button3.setLayoutY(230);
 
@@ -80,9 +80,32 @@ public class Study7_Button extends Application {
             @Override
             public void handle(MouseEvent event) {
                 System.out.println("测试按钮被点击了");
-                if(event.getClickCount() == 2){
+
+                // 左键name=PRIMARY 右键name=SECONDARY 中间键name=MIDDLE
+                System.out.println("鼠标按键 = " + event.getButton().name());
+                if (event.getClickCount() == 2 && event.getButton().name().equals(MouseButton.PRIMARY.name())) {
                     System.out.println("双击");
                 }
+            }
+        });
+
+        Button button4 = new Button("监听键盘按键事件");
+        button4.setLayoutX(100);
+        button4.setLayoutY(260);
+
+        button4.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                System.out.println("按键按下");
+                //获取按下按键的名称
+                System.out.println(event.getCode().getName());
+            }
+        });
+        button4.setOnKeyReleased(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                System.out.println("按键释放");
+                System.out.println(event.getCode().getName());
             }
         });
 
@@ -91,6 +114,7 @@ public class Study7_Button extends Application {
         group.getChildren().add(button);
         group.getChildren().add(button2);
         group.getChildren().add(button3);
+        group.getChildren().add(button4);
 
         Scene scene = new Scene(group);
 
