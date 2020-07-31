ec91170cbadb1d787121d70a2e8279586f3d9354
Edik <wfj@edik.cn>
2020-07-31 17:36:48 +0800

    Study9_Input

--- src/main/java/cn/edik/Study10_Layout_AnchorPane.java
+++ src/main/java/cn/edik/Study10_Layout_AnchorPane.java
@@ -9,6 +9,7 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
@@ -19,14 +20,28 @@ public class Study10_Layout_AnchorPane extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-        Group group = new Group();
-        Scene scene = new Scene(group);
+        AnchorPane anchorPane = new AnchorPane();
+        anchorPane.setStyle("-fx-background-color: #E5E7E9;");
+        anchorPane.setFocusTraversable(false);
+        Scene scene = new Scene(anchorPane);
 
         stage.setTitle("JavaFx布局之AnchorPane");
         stage.setWidth(800);
         stage.setHeight(500);
         stage.setScene(scene);
 
+
+        Button button = new Button("bt1");
+        AnchorPane.setTopAnchor(button,10.0);
+        AnchorPane.setLeftAnchor(button,10.0);
+        button.setFocusTraversable(false);
+
+        Button button1 = new Button("bt2");
+        AnchorPane.setTopAnchor(button1,10.0);
+        AnchorPane.setLeftAnchor(button1,50.0);
+        button1.setFocusTraversable(false);
+
+        anchorPane.getChildren().addAll(button,button1);
 
 
         stage.show();
